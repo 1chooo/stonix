9873ab87a715b01f4aeeb8fb0446c0bef47ba232
1chooo <hugo970217@gmail.com>
2024-03-21 18:50:43 +0800

    add: login test

--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/1chooo/trade-tracker/handlers"
+	"github.com/1chooo/trade-tracker/handlers"
+	"github.com/gin-gonic/gin"
 )
 
+type IndexData struct {
+	Title   string
+	Content string
+}
+
+func test(ctx *gin.Context) {
+	data := new(IndexData)
+	data.Title = "Trade Tracker"
+	data.Content = "Welcome to the home page."
+	ctx.HTML(http.StatusOK, "index.html", data)
+}
+
 func main() {
-    router := gin.Default()
-    router.LoadHTMLFiles("./views/index.html")
+	router := gin.Default()
+	router.LoadHTMLFiles("./templates/index.html", "./templates/login.html")
+	router.Static("/assets", "./templates/assets")
 
-    router.GET("/", func(ctx *gin.Context) {
-        ctx.HTML(http.StatusOK, "index.html", nil)
-    })
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	ctx.HTML(http.StatusOK, "index.html", nil)
+	// })
+	router.GET("/", test)
+	router.GET("/login", handlers.LoginPage)
+    router.POST("/login", handlers.LoginAuth)
 
-    router.GET("/books", handlers.GetBooks)
-    router.POST("/books", handlers.CreateBook)
-    router.PATCH("/checkout", handlers.CheckoutBook)
-    router.PATCH("/return", handlers.ReturnBook)
-    router.GET("/welcome", func(ctx *gin.Context) {
-        firstname := ctx.DefaultQuery("firstname", "Guest")
-        lastname := ctx.Query("lastname") // shortcut for ctx.Request.URL.Query().Get("lastname")
+	router.GET("/books", handlers.GetBooks)
+	router.POST("/books", handlers.CreateBook)
+	router.PATCH("/checkout", handlers.CheckoutBook)
+	router.PATCH("/return", handlers.ReturnBook)
+	router.GET("/welcome", func(ctx *gin.Context) {
+		firstname := ctx.DefaultQuery("firstname", "Guest")
+		lastname := ctx.Query("lastname") // shortcut for ctx.Request.URL.Query().Get("lastname")
 
-        ctx.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-    })
-    router.Run("localhost:8080")
+		ctx.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+	router.Run("localhost:8080")
 }
