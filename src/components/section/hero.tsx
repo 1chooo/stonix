7802db4dd94db12f4cbeb81e58bdf7563561d600
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-03-02 02:19:40 +0800

    feat(hero): integrate AuroraText and TypingAnimation components for enhanced text display

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,7 +1,7 @@
-"use client"
-
 import { useTranslations } from "next-intl"
 
+import { AuroraText } from "@/components/magicui/aurora-text";
+import { TypingAnimation } from "@/components/magicui/typing-animation";
 
 export default function Hero() {
   const t = useTranslations("HomePage")
@@ -10,18 +10,9 @@ export default function Hero() {
     <div className="space-y-8">
       <div className="flex flex-col items-center justify-center w-full h-full z-3">
         <h1 className="text-center text-4xl font-bold sm:text-8xl">
-          {t("title")}{" "}
-          <span
-            className="block text-6xl sm:text-8xl sm:inline-block bg-gradient-to-r from-blue-500 to-purple-700 bg-clip-text text-transparent"
-          >
-            {t("project")}
-          </span>
+          {t("title")}{" "} <AuroraText>{t("project")}</AuroraText>
         </h1>
-        <h2
-          className="px-4 py-6 text-center text-lg md:text-xl text-gh-text-secondary"
-        >
-          {t("subtitle")}
-        </h2>
+        <TypingAnimation className="px-4 py-6 text-center text-lg md:text-xl text-gh-text-secondary">{t("subtitle")}</TypingAnimation>
       </div>
       <p className="leading-6 text-muted-foreground text-center max-w-2xl mx-auto text-lg md:text-xl">
         {t("description")}
