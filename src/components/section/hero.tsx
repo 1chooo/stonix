848551149df63b4e4e7d714e127a91fd3242a073
Thomas Wu <62056970+TakumiBC@users.noreply.github.com>
2025-03-07 20:57:14 +0800

    Update hero.tsx

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -3,7 +3,18 @@
 import { useTranslations } from "next-intl";
 
 export default function Hero() {
-@@ -18,9 +16,4 @@ export default function Hero() {
+  const t = useTranslations("HomePage");
+
+  return (
+    <div className="space-y-8">
+      <div className="z-3 flex h-full w-full flex-col items-center justify-center">
+        <h1 className="text-center text-4xl font-bold sm:text-8xl">
+          {t("title")}{" "}
+          <span className="block bg-gradient-to-r from-blue-500 to-purple-700 bg-clip-text text-6xl text-transparent sm:inline-block sm:text-8xl">
+            {t("project")}
+          </span>
+        </h1>
+        <h2 className="text-gh-text-secondary px-4 py-6 text-center text-lg md:text-xl">
           {t("subtitle")}
         </h2>
       </div>
